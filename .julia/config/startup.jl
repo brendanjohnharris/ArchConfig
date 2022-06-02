af051262afdbfba6b390396629040216e32e23f7
brendanjohnharris <brendanjohnharris@gmail.com>
2022-06-02 15:13:20 +1000

    Update julia package template

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,6 +1,18 @@
 function template()
     @eval begin
         using PkgTemplates
-        Template(; #= ... =#)
+        Template(;  user="brendanjohnharris",
+                    dir="./",
+                    julia=v"1.5.0",
+                    plugins=[   ProjectFile(),
+                                SrcDir(),
+                                Tests(; project=true),
+                                Readme(),
+                                License(),
+                                Git(;ignore=["*.code-workspace"]),
+                                CompatHelper(),
+                                TagBot(),
+                                GitHubActions(),
+                                Codecov()])
     end
 end
