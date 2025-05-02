c4cfa369892ca0d4b4f35e2c407cb1dea0975ebf
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2025-05-02 12:33:59 +1000

    System backup

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,57 +1,58 @@
-pkgs = [:Pkg,
-    :Revise,
-    :OhMyREPL,
-    :Infiltrator,
-    :Downloads,
-    :TestEnv,
-    :PkgTemplates]
+using Pkg, Revise, OhMyREPL, Infiltrator, Downloads, TestEnv, PkgTemplates
+# pkgs = [:Pkg,
+#     :Revise,
+#     :OhMyREPL,
+#     :Infiltrator,
+#     :Downloads,
+#     :TestEnv,
+#     :PkgTemplates]
 
-printout = false
-if false
-    using Term
-    function make_line(pkg, time)
-        status = isnothing(time) ? "{bold yellow}•{/bold yellow}" : "{bold green}✔{/bold green}"
-        out = status * " $pkg"
-        if !isnothing(time)
-            time, compile, recompile = time
-            out = out * ": $(time)s ($(compile)s compile, $(recompile)s recompile)"
-        end
-        return out
-    end
-    function _make_panel(pkgs, times)
-        out = map(pkgs, times) do pkg, time
-            make_line(pkg, time)
-        end
-        out = ["{bold bright_blue}Startup packages{/bold bright_blue}", out...]
-        out = join(out, "\n")
-    end
-    function make_panel(pkgs, times)
-        out = _make_panel(pkgs, times)
-        Term.TextWidget(out; as_panel=false)
-    end
-    times = fill(nothing, length(pkgs)) |> Vector{Any}
-    app = Term.App(make_panel(pkgs, times))
-    display(app |> Term.frame)
-    map(pkgs, times) do pkg, time
-        t = @timed eval(:(using $pkg))
-        ts = getindex.([t], [:time, :compile_time, :recompile_time])
-        ts = round.(ts; sigdigits=1)
-        times[pkgs.==pkg] .= [ts]
-        Term.LiveWidgets.erase!(app)
-        app.widgets[:A].text = _make_panel(pkgs, times)
-    end
-    return nothing
-else
-    map(pkgs) do pkg
-        printout && print("$pkg: ")
-        t = @timed eval(:(using $pkg))
-        time = round(t[:time]; sigdigits=1)
-        compile = round(t[:compile_time]; sigdigits=1)
-        recompile = round(t[:recompile_time]; sigdigits=1)
-        printout && print("$(time)s ($(compile)s compile, $(recompile)s recompile)\n")
-    end
-    return nothing
-end
+# printout = false
+# if false
+#     using Term
+#     function make_line(pkg, time)
+#         status = isnothing(time) ? "{bold yellow}•{/bold yellow}" : "{bold green}✔{/bold green}"
+#         out = status * " $pkg"
+#         if !isnothing(time)
+#             time, compile, recompile = time
+#             out = out * ": $(time)s ($(compile)s compile, $(recompile)s recompile)"
+#         end
+#         return out
+#     end
+#     function _make_panel(pkgs, times)
+#         out = map(pkgs, times) do pkg, time
+#             make_line(pkg, time)
+#         end
+#         out = ["{bold bright_blue}Startup packages{/bold bright_blue}", out...]
+#         out = join(out, "\n")
+#     end
+#     function make_panel(pkgs, times)
+#         out = _make_panel(pkgs, times)
+#         Term.TextWidget(out; as_panel=false)
+#     end
+#     times = fill(nothing, length(pkgs)) |> Vector{Any}
+#     app = Term.App(make_panel(pkgs, times))
+#     display(app |> Term.frame)
+#     map(pkgs, times) do pkg, time
+#         t = @timed eval(:(using $pkg))
+#         ts = getindex.([t], [:time, :compile_time, :recompile_time])
+#         ts = round.(ts; sigdigits=1)
+#         times[pkgs.==pkg] .= [ts]
+#         Term.LiveWidgets.erase!(app)
+#         app.widgets[:A].text = _make_panel(pkgs, times)
+#     end
+#     return nothing
+# else
+#     map(pkgs) do pkg
+#         printout && print("$pkg: ")
+#         t = @timed eval(:(using $pkg))
+#         time = round(t[:time]; sigdigits=1)
+#         compile = round(t[:compile_time]; sigdigits=1)
+#         recompile = round(t[:recompile_time]; sigdigits=1)
+#         printout && print("$(time)s ($(compile)s compile, $(recompile)s recompile)\n")
+#     end
+#     return nothing
+# end
 
 
 
