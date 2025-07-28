a6fde95a11a930c91ddbebf89e6e97c3777fcfdf
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2025-07-28 11:42:55 +1000

    System backup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 starship init fish | source
 
-function config; git --git-dir="$HOME/ArchConfig" --work-tree="$HOME" $argv; end
-function qload; ssh headnode.physics.usyd.edu.au -t "qload"; end
-function monitor; ssh headnode.physics.usyd.edu.au -t "monitor"; end
+function config
+    git --git-dir="$HOME/ArchConfig" --work-tree="$HOME" $argv
+end
 
 set fish_greeting ""
 
@@ -35,18 +35,15 @@ set fish_pager_color_progress brwhite --background=cyan
 
 set -Ux CRYPTOGRAPHY_OPENSSL_NO_LEGACY 1
 
-
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /home/brendan/miniconda3/bin/conda
-    eval /home/brendan/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    eval /home/brendan/miniconda3/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/home/brendan/miniconda3/etc/fish/conf.d/conda.fish"
         . "/home/brendan/miniconda3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/home/brendan/miniconda3/bin" $PATH
+        set -x PATH /home/brendan/miniconda3/bin $PATH
     end
 end
 # <<< conda initialize <<<
-
